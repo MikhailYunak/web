0cd33018b60c3278db8bdae2c9c21cda0c85725d
Mikhail <36512557+MikhailYunak@users.noreply.github.com>
2020-02-05 14:01:05 +0200

    Create sum_of_range.js

---
+++ Js Task/sum_of_range.js
@@ -0,0 +1,33 @@
+function range(start, end, step = start < end? 1 : -1) {
+    let myArr = [];
+
+    if (step > 0) {
+        for (let i = start; i <= end; i += step) {
+            myArr.push(i);
+        }
+    } else {
+        for (let i = start; i >= end; i += step) {
+            myArr.push(i);
+        }
+    }
+    return myArr;
+}
+
+console.log(range(5, 2, -2));
+
+function sum(arr = []) {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return arr.reduce(reducer, 0);
+}
+
+console.log(sum(range(1, 10)));
+
+function sum1(arr = []) {
+    let total = 0;
+    for (let value of arr){
+        total += value;
+    }
+    return total;
+}
+
+console.log(sum1(range(1, 10)));
